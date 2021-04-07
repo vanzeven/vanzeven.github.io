46b6eca7807d3de45de0f85b5b8879b23f100204
Faisal Reza <7farema@gmail.com>
2021-04-07 20:45:56 +0700

    Update seprovi.php

--- seprovi.php
+++ seprovi.php
@@ -4,8 +4,8 @@ $update = json_decode(file_get_contents("php://input"), TRUE);
 $chatId = $update["message"]["chat"]["id"];
 $message = $update["message"]["text"];
 if (strpos($message, "/weather") === 0) {
-	$location = substr($message, 9);
-};
-$weather = json_decode(file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=".$location."&appid=mytoken"), TRUE)["weather"]["main"];
-file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=Here's the weather in ".$location.": ". $.weather);
+$location = substr($message, 9);
+$weather = json_decode(file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=".$location."&appid=mytoken"), TRUE)["weather"][0]["main"];
+file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=Here's the weather in ".$location.": ". $weather);
+}
 ?>
