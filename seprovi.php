f019bd42bf813df76784eafeae5e9ec24e97bef8
Faisal Reza <7farema@gmail.com>
2021-04-07 20:43:46 +0700

    Update seprovi.php

--- seprovi.php
+++ seprovi.php
@@ -1,4 +1,4 @@
-<?php ?>
+<?php
 $path = "https://api.telegram.org/bot1777224326:AAFe6Stgk8BFGnAl3hohQZpzcgYJzagccA0;
 $update = json_decode(file_get_contents("php://input"), TRUE);
 $chatId = $update["message"]["chat"]["id"];
@@ -8,3 +8,4 @@ if (strpos($message, "/weather") === 0) {
 };
 $weather = json_decode(file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=".$location."&appid=mytoken"), TRUE)["weather"]["main"];
 file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=Here's the weather in ".$location.": ". $.weather);
+?>
